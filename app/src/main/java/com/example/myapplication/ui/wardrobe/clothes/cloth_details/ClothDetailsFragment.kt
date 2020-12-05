1652716e8d0de8a1bf79d5a0aba050c7f9160547
DESKTOP-JV6VGDO\ilsha <ilshat.fatkhullin@gmail.com>
2020-12-05 17:06:27 +0300

    Added authorization navigation pipeline.

---
+++ app/src/main/java/com/example/myapplication/ui/wardrobe/clothes/cloth_details/ClothDetailsFragment.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication.ui.wardrobe.clothes.cloth_details
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+
+class ClothDetailsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ClothDetailsFragment()
+    }
+
+    private lateinit var viewModel: ClothDetailsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_cloth_details, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ClothDetailsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
