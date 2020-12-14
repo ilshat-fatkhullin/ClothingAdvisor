438f3d66275f569e461b9dcbcab9575f6268d54a
Daniil Dvoryanov <daniil.dvoryanov@gmail.com>
2020-12-14 22:26:21 +0200

    merge local with master

--- app/src/main/java/com/group5/clothing_advisor/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/group5/clothing_advisor/ui/profile/ProfileViewModel.kt
@@ -1,7 +1,36 @@
 package com.group5.clothing_advisor.ui.profile
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+private const val TAG = "ProfileViewModel"
 
 class ProfileViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _userName = MutableLiveData<String?>("<Unauthorized>")
+    val userName: LiveData<String?> get() = _userName
+
+    private val _isVerified = MutableLiveData(true)
+    val isVerified: LiveData<Boolean> get() = _isVerified
+
+    private lateinit var currUser: FirebaseUser
+
+    init {
+        val user = FirebaseAuth.getInstance().currentUser
+        if(user == null){
+            Log.wtf(TAG, "The user is not authenticated, he should not have access to profile!")
+        }
+        else {
+            currUser = user
+            _userName.postValue(user.displayName)
+            _isVerified.postValue(user.isEmailVerified)
+        }
+    }
+
+    fun sendVerificationLink() {
+        currUser.sendEmailVerification()
+    }
 }
